c6dad766a4972bb21fb42b297726447a05a23689
info@ralfw.de <info@ralfw.de@781cd7b2-be3c-0410-81b4-87051234213d>
2008-01-04 11:21:22 +0000

    implemented midi to xml conversion

---
+++ test.whitebox.midifileadapter/testConverter.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Xml;
+
+using NUnit.Framework;
+
+using dnppv.midifileadapter;
+
+namespace test.whitebox.midifileadapter
+{
+    [TestFixture]
+    public class testConverter
+    {
+        [Test]
+        public void testStartConverterInBackground()
+        {
+            Midi2XmlConverter c = new Midi2XmlConverter();
+            XmlDocument xmlMid = c.Convert(@"..\..\test1-kling.mid");
+            Assert.AreEqual("MIDIFile", xmlMid.DocumentElement.Name);
+        }
+    }
+}
